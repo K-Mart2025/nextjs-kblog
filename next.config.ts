db92b324dd38d406774786c184a7c918eb6e4c50
Anthiel04 <richardwmh04@gmail.com>
2025-11-01 08:00:05 -0400

    Updated nextconfig

--- next.config.ts
+++ next.config.ts
@@ -2,18 +2,22 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '3000',
-        pathname: '/uploads/**',
+        protocol: "http",
+        hostname: "localhost",
+        port: "3000",
+        pathname: "/uploads/**",
       },
       {
-        protocol: 'https',
-        hostname: 'images.pexels.com',
+        protocol: "https",
+        hostname: "images.pexels.com",
+      },
+      {
+        protocol: "https",
+        hostname: "kmarthabana.run.place",
       },
     ],
   },
-  output: 'standalone',
+  output: "standalone",
 };
 
 export default nextConfig;
